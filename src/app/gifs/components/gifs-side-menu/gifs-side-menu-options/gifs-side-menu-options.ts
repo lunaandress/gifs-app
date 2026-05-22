fec1d9af559b2f567fe61a51bc827a53778931ca
Andres Luna <miosesfreddy123@gmail.com>
2026-05-22 16:09:21 +0200

    feature/Routerlink-routerLinkActive

--- src/app/gifs/components/gifs-side-menu/gifs-side-menu-options/gifs-side-menu-options.ts
+++ src/app/gifs/components/gifs-side-menu/gifs-side-menu-options/gifs-side-menu-options.ts
@@ -1,8 +1,36 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
+
+
+interface MenuOpcion{
+  label:string;
+  sublabel:string;
+  router:string;
+  icon: string;
+}
+
 
 @Component({
   selector: 'app-gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink,RouterLinkActive],
   templateUrl: './gifs-side-menu-options.html',
 })
-export class GifsSideMenuOptions {}
+
+
+export class GifsSideMenuOptions {
+
+  menuOptions:MenuOpcion[]=[
+    {
+      icon:'fa-solid fa-chart-line',
+      label:'Trending',
+      sublabel:'Gifs Populary',
+      router:'/dashboard/trending'
+    },
+        {
+      icon:'fa-solid fa-magnifying-glass',
+      label:'Buscdor',
+      sublabel:'Buscador gifs',
+      router:'/dashboard/search'
+    },
+];
+}
