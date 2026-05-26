88be28b58e6e77b17b7fa131f939fedd4827a982
Andres Luna <miosesfreddy123@gmail.com>
2026-05-26 16:03:21 +0200

    feature/environments-pathAlias

--- src/app/gifs/components/gifs-side-menu/gifs-side-menu-header/gifs-side-menu-header.ts
+++ src/app/gifs/components/gifs-side-menu/gifs-side-menu-header/gifs-side-menu-header.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from '@environments/environment';
 
 @Component({
   selector: 'app-gifs-side-menu-header',
@@ -8,4 +9,5 @@ import { Component } from '@angular/core';
 })
 export class GifsSideMenuHeaderComponent {
 
+  envs= environment;
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  productions:true,
+  companyName:"Gifs",
+  companyName2:"App",
+  companySlogan:"Maneja tus gifs"
+
+  //ApiKeys
+
+
+  //URLS
+
+
+};
+
+
